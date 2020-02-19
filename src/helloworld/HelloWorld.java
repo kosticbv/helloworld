068b3646ddfcee63e11faadba2f32f2e1815dc73
kosticbv <kosticbv@gmail.com>
2020-02-19 21:18:09 +0100

    First commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world! :)");  //sout+ctrl+space, syso+ctrl+space
+		System.out.println("Tamara jel znas javu?");
+		System.out.println("Znam");
+		System.out.println("Svaka cast");
+		System.out.print("Bozo kako ti ide broj telefona?"); // run = ctrl + f11
+		System.out.println(" 0606988691");                   // save with ctrl + s
+		
+	}
+
+}
